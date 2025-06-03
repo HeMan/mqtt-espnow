99450d8de8391a62f3937021e2bde42e7f23b382
Jimmy Hedman <jimmy.hedman@gmail.com>
2025-06-03 14:16:50 +0200

    Enable ESP-NOW and ethernet with IPv6

--- src/hello_world/main.c
+++ src/hello_world/main.c
@@ -1,13 +1,136 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "nvs_flash.h"
+
 #include "esp_system.h"
 #include "esp_chip_info.h"
 #include "esp_flash.h"
 
+#include "esp_eth.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "esp_system.h"
+#include "esp_err.h"
+#include "esp_eth_phy.h"
+#include "esp_eth_mac.h"
+#include "esp_eth_netif_glue.h"
+#include "esp_eth_driver.h"
+#include "esp_netif_types.h"
+#include "esp_netif_ip_addr.h"
+#include "esp_netif_defaults.h"
+
+#include "esp_now.h"
+#include "esp_wifi.h"
+
+#define ETH_MDC_PIN 23
+#define ETH_MDIO_PIN 18
+#define ETH_CLK_MODE ETH_CLOCK_GPIO17_OUT
+
+static const char *TAG = "ethernet_init";
+void eth_event_handler(void *arg, esp_event_base_t event_base,
+                       int32_t event_id, void *event_data)
+{
+    if (event_id == ETHERNET_EVENT_CONNECTED)
+    {
+        ESP_LOGI(TAG, "Ethernet Link Up");
+    }
+    else if (event_id == ETHERNET_EVENT_DISCONNECTED)
+    {
+        ESP_LOGI(TAG, "Ethernet Link Down");
+    }
+    else if (event_id == ETHERNET_EVENT_START)
+    {
+        ESP_LOGI(TAG, "Ethernet Started");
+    }
+    else if (event_id == ETHERNET_EVENT_STOP)
+    {
+        ESP_LOGI(TAG, "Ethernet Stopped");
+    }
+}
+
+void got_ip_event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data)
+{
+    ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+    ESP_LOGI(TAG, "Got IP Address:" IPSTR, IP2STR(&event->ip_info.ip));
+}
+
+void got_ip6_event_handler(void *arg, esp_event_base_t event_base,
+                           int32_t event_id, void *event_data)
+{
+    ip_event_got_ip6_t *event = (ip_event_got_ip6_t *)event_data;
+    ESP_LOGI(TAG, "Got IPv6 Address: " IPV6STR, IPV62STR(event->ip6_info.ip));
+}
+
+void init_ethernet(void)
+{
+
+    esp_netif_config_t cfg = ESP_NETIF_DEFAULT_ETH();
+    esp_netif_t *eth_netif = esp_netif_new(&cfg);
+
+    ESP_ERROR_CHECK(esp_event_handler_register(ETH_EVENT, ESP_EVENT_ANY_ID, &eth_event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &got_ip_event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_GOT_IP6, &got_ip6_event_handler, NULL));
+
+    eth_mac_config_t mac_config = ETH_MAC_DEFAULT_CONFIG();
+    eth_phy_config_t phy_config = ETH_PHY_DEFAULT_CONFIG();
+    eth_esp32_emac_config_t esp32_emac_config = ETH_ESP32_EMAC_DEFAULT_CONFIG();
+    esp32_emac_config.smi_gpio.mdc_num = ETH_MDC_PIN;
+    esp32_emac_config.smi_gpio.mdio_num = ETH_MDIO_PIN;
+    esp32_emac_config.clock_config.rmii.clock_mode = EMAC_CLK_OUT;
+    esp32_emac_config.clock_config.rmii.clock_gpio = EMAC_CLK_OUT_180_GPIO;
+
+    esp_eth_mac_t *mac = esp_eth_mac_new_esp32(&esp32_emac_config, &mac_config);
+
+    esp_eth_phy_t *phy = esp_eth_phy_new_lan87xx(&phy_config);
+
+    esp_eth_handle_t eth_handle = NULL;
+    esp_eth_config_t eth_config = ETH_DEFAULT_CONFIG(mac, phy);
+    ESP_ERROR_CHECK(esp_eth_driver_install(&eth_config, &eth_handle));
+    ESP_ERROR_CHECK(esp_netif_attach(eth_netif, esp_eth_new_netif_glue(eth_handle)));
+    ESP_ERROR_CHECK(esp_eth_start(eth_handle));
+    ESP_ERROR_CHECK(esp_netif_set_hostname(eth_netif, "mqtt-espnow"));
+    ESP_ERROR_CHECK(esp_netif_create_ip6_linklocal(eth_netif));
+}
+
+void init_espnow(void)
+{
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(ESP_IF_WIFI_AP));
+
+    ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_ERROR_CHECK(esp_now_init());
+    ESP_LOGI("espnow_init", "ESP-NOW initialized");
+}
+
+void init_system(void)
+{
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+}
+
 void app_main(void)
 {
-    printf("Hello world!\n");
+    init_system();
+    init_espnow();
+    printf("ESP-NOW initialized. Waiting for connection...\n");
+
+    init_ethernet();
+    printf("Ethernet initialized. Waiting for connection...\n");
 
     /* Print chip information */
     esp_chip_info_t chip_info;
@@ -24,6 +147,8 @@ void app_main(void)
     esp_flash_get_size(NULL, &flash_size);
     printf("%luMB %s flash\n", flash_size / (1024 * 1024),
            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    printf("Hello world!\n");
 
     for (int i = 10; i >= 0; i--)
     {
