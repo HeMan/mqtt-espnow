4a8ba4c5ff6b5b22cc180ec227ecd3d50d18ef89
Jimmy Hedman <jimmy.hedman@gmail.com>
2025-06-11 22:32:08 +0200

    Publishes static data to mqtt broker

--- src/mqtt-espnow/main.c
+++ src/mqtt-espnow/main.c
@@ -26,11 +26,32 @@
 #include "esp_now.h"
 #include "esp_wifi.h"
 
+#include "mqtt_client.h"
+
 #define ETH_MDC_PIN 23
 #define ETH_MDIO_PIN 18
 #define ETH_CLK_MODE ETH_CLOCK_GPIO17_OUT
 
+static uint8_t s_example_broadcast_mac[ESP_NOW_ETH_ALEN] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+#define IS_BROADCAST_ADDR(addr) (memcmp(addr, s_example_broadcast_mac, ESP_NOW_ETH_ALEN) == 0)
+
 static const char *TAG = "ethernet_init";
+
+void init_system(void)
+{
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+}
+
 void eth_event_handler(void *arg, esp_event_base_t event_base,
                        int32_t event_id, void *event_data)
 {
@@ -96,6 +117,30 @@ void init_ethernet(void)
     ESP_ERROR_CHECK(esp_netif_set_hostname(eth_netif, "mqtt-espnow"));
     ESP_ERROR_CHECK(esp_netif_create_ip6_linklocal(eth_netif));
 }
+static void example_espnow_recv_cb(const esp_now_recv_info_t *recv_info, const uint8_t *data, int len)
+{
+    uint8_t *mac_addr = recv_info->src_addr;
+    uint8_t *des_addr = recv_info->des_addr;
+
+    if (mac_addr == NULL || data == NULL || len <= 0)
+    {
+        ESP_LOGE(TAG, "Receive cb arg error");
+        return;
+    }
+
+    if (IS_BROADCAST_ADDR(des_addr))
+    {
+        /* If added a peer with encryption before, the receive packets may be
+         * encrypted as peer-to-peer message or unencrypted over the broadcast channel.
+         * Users can check the destination address to distinguish it.
+         */
+        ESP_LOGD(TAG, "Receive broadcast ESPNOW data");
+    }
+    else
+    {
+        ESP_LOGD(TAG, "Receive unicast ESPNOW data");
+    }
+}
 
 void init_espnow(void)
 {
@@ -106,31 +151,96 @@ void init_espnow(void)
 
     ESP_ERROR_CHECK(esp_wifi_start());
     ESP_ERROR_CHECK(esp_now_init());
+    ESP_ERROR_CHECK(esp_now_register_recv_cb(example_espnow_recv_cb));
+
     ESP_LOGI("espnow_init", "ESP-NOW initialized");
 }
 
-void init_system(void)
-{
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
-    {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
+static const char *MQTT_TAG = "mqtt_client";
 
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
+esp_mqtt_client_handle_t mqtt_client = NULL;
+
+static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
+{
+    switch (event->event_id)
+    {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(MQTT_TAG, "MQTT_EVENT_CONNECTED");
+        esp_mqtt_client_subscribe(event->client, "/espnow/test", 0);
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(MQTT_TAG, "MQTT_EVENT_DISCONNECTED");
+        break;
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(MQTT_TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(MQTT_TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(MQTT_TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(MQTT_TAG, "MQTT_EVENT_DATA");
+        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+        printf("DATA=%.*s\r\n", event->data_len, event->data);
+        break;
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(MQTT_TAG, "MQTT_EVENT_ERROR");
+        break;
+    default:
+        ESP_LOGI(MQTT_TAG, "Other event id:%d", event->event_id);
+        break;
+    }
+    return ESP_OK;
+}
+
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    mqtt_event_handler_cb(event_data);
+}
+
+void init_mqtt(void)
+{
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .broker.address.uri = "mqtt://broker.hivemq.com",
+    };
+
+    mqtt_client = esp_mqtt_client_init(&mqtt_cfg);
+    ESP_ERROR_CHECK(esp_mqtt_client_register_event(mqtt_client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL));
+    ESP_ERROR_CHECK(esp_mqtt_client_start(mqtt_client));
 }
 
 void app_main(void)
 {
+    int msg_id = 0;
     init_system();
     init_espnow();
     printf("ESP-NOW initialized. Waiting for connection...\n");
 
     init_ethernet();
     printf("Ethernet initialized. Waiting for connection...\n");
+
+    vTaskDelay(3000 / portTICK_PERIOD_MS);
+    // Log DNS server(s) after Ethernet got IP
+    esp_netif_dns_info_t dns_info;
+    if (esp_netif_get_dns_info(esp_netif_get_handle_from_ifkey("ETH_DEF"), ESP_NETIF_DNS_MAIN, &dns_info) == ESP_OK)
+    {
+        if (dns_info.ip.type == ESP_IPADDR_TYPE_V4)
+        {
+            ESP_LOGI(TAG, "DNS Server: " IPSTR, IP2STR(&dns_info.ip.u_addr.ip4));
+        }
+        else if (dns_info.ip.type == ESP_IPADDR_TYPE_V6)
+        {
+            ESP_LOGI(TAG, "DNS Server: " IPV6STR, IPV62STR(dns_info.ip.u_addr.ip6));
+        }
+    }
+    else
+    {
+        ESP_LOGI(TAG, "No DNS server info available");
+    }
+    init_mqtt();
+    printf("MQTT client initialized. Waiting for connection...\n");
 
     /* Print chip information */
     esp_chip_info_t chip_info;
@@ -149,13 +259,20 @@ void app_main(void)
            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
     printf("Hello world!\n");
+    while (1)
+    {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        printf("Running...\n");
+        msg_id = esp_mqtt_client_publish(mqtt_client, "/topic/qos1", "data_3", 0, 1, 0);
+        ESP_LOGI("mqtt-publish", "sent publish successful, msg_id=%d", msg_id);
+    }
 
-    for (int i = 10; i >= 0; i--)
+    /*for (int i = 10; i >= 0; i--)
     {
         printf("Restarting in %d seconds...\n", i);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
     printf("Restarting now.\n");
     fflush(stdout);
-    esp_restart();
+    esp_restart();*/
 }
